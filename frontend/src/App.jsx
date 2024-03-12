1cb47622d1531c81713f4afb61ee93d901623192
Aman Kumar <cs21b001@iittp.ac.in>
2024-03-12 20:04:42 +0530

    added navbar as global component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './components/Home';
+import Navbar from './components/Navbar';
+import DependencyTracker from './components/DependencyTracker';
 
 const App = () => {
     return (
         <BrowserRouter>
+            <Navbar />
             <Routes>
                 <Route path='/' element={<Home />}/>
+                <Route path="/components/DependencyTracker" element={<DependencyTracker/>} />
             </Routes>
         </BrowserRouter>
     );
