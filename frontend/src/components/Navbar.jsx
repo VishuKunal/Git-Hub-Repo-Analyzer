f4480beefe31eb9ecbcff4a3d0230d1438dee852
Aman Kumar <cs21b001@iittp.ac.in>
2024-03-12 20:05:29 +0530

    linked Dependency tracker

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import '../styles/Navbar.css';
+import { Link } from 'react-router-dom';
 
 const Navbar = () => {
     return (
         <div className="navbarContainer">
             <ul>
-                <li>Dependency Tracker</li>
+                <li><Link to="/components/DependencyTracker">Dependency Tracker</Link></li>
                 <li>Others</li>
                 <li>About Us</li>
                 <li>Home</li>
