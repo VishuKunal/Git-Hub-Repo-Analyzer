c9500859a6f22ce291a46de060474c3edd95face
Aman Kumar <cs21b001@iittp.ac.in>
2024-03-12 20:05:02 +0530

    removed navbar

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,11 +1,9 @@
 import React from "react";
-import Navbar from './Navbar';
 import DependencyTracker from "./DependencyTracker";
 
 const Home = () => {
     return (
         <div className="homeContainer">
-            <Navbar />
             <DependencyTracker />
         </div>
     );
